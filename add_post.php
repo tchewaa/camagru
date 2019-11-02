5e66b77c29b078cfd069214055f9c5d9e378c6ee
Thabang <thabang.chewa@gmail.com>
2019-11-02 17:23:03 +0200

    added a add post page

---
+++ add_post.php
@@ -0,0 +1,36 @@
+
+<?php
+    require "includes/header.php";
+?>
+<main>
+        <section class="timeline_section">
+            <div class="container">
+                <div class="row">
+                    <div class="profile_container">
+                        <h4 class="right">Add a Post</h4>
+
+                        <p><span class="error"><?php if (isset($error)) echo $error ?></span>
+                        <span class="success"><?php if (isset($success)) echo $success ?></span></p><br/>
+                        <form enctype="multipart/form-data" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                            <label>Upload an Image:</label><br/><br/>
+                            <input type='file' name='file' /><br/><br/>
+                            <textarea rows="6" placeholder="Caption"></textarea><br/><br/>
+                            
+                            <button style="width: 10%" class ="primary-button" type="submit" name="">Post</button>
+                        </form>
+                        
+
+                        
+                       
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+</main>
+<?php
+    require "includes/footer.php";
+?>
+</body>
+
+</html>
