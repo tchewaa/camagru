a9e01eed2ebd3ada0e36b4f87d2ba50e359bf84a
Thabang <thabang.chewa@gmail.com>
2019-11-03 17:49:01 +0200

    added edit profile with receive email

--- edit_profile.php
+++ edit_profile.php
@@ -1,6 +1,31 @@
 
 <?php
-    require "includes/header.php";
+    require("includes/header.php");
+    if (isset($_POST['save']))
+    {
+        $fullname = $_POST['fullname'];
+        $username = $_POST['username'];
+        $email_address = $_POST['email_address'];
+
+        if (empty($fullname) || empty($username) || empty($email_address))
+        {
+            $error = "Error: Empty fields not allowed.";
+        }
+        else if (!filter_var($email_address, FILTER_VALIDATE_EMAIL)){
+            $error = "Invalid email address.";
+        } 
+        else {
+            
+            try{
+                $sql = "UPDATE `users` SET `fullname` = '".$fullname."', `username` = '".$username."', `email_address` = '".$email_address."' WHERE `user_id` = '".$_SESSION["user_id"]."'";
+                $conn->exec($sql);
+                $success = "Updated Successfully.";
+            }
+            catch(PDOException $e){
+                $error = "Error: ".$e->getMessage();
+            }
+        }
+    }
 ?>
 <main>
         <section class="timeline_section">
@@ -9,23 +34,26 @@
                     <div class="profile_container">
                         <h4 class="right">Edit Profile</h4>
                        <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                                <?php
-                                    if (isset($_SESSION['username']))
-                                        echo $_SESSION['username'];
-                                    else if(isset($_SESSION['profile_pic']))
-                                            echo $_SESSION["profile_pic"];
-                                    else
-                                            echo '<img src="images/no-profile-pic-icon-5.jpg"/>';
-                                ?>
+                                <br/>
                                  <p><span class="error"><?php if (isset($error)) echo $error ?></span>
                                 <span class="success"><?php if (isset($success)) echo $success ?></span></p><br/>
                                 Name<br/><input type="text" value = "<?php if(isset($_SESSION['fullname'])){echo $_SESSION['fullname'];}?>" name="fullname" placeholder="Full Name"><br/><br/>
-                                Username<br/><input type="text" value = "<?php if(isset($_SESSION['username'])){echo $_SESSION['username'];}?>" name="username" placeholder="Username"><br/><br/>
-                                Website<br/><input type="text" value = "<?php if(isset($_SESSION['website'])){echo $_SESSION['website'];}?>" name="website" placeholder="website"><br/><br/>
-                                Bio<br/><textarea rows="6" placeholder="Bio"><?php if(isset($_SESSION['bio'])){echo $_SESSION['bio'];}?></textarea><br/><br/>
+                                Username<br/><input readonly type="text" value = "<?php if(isset($_SESSION['username'])){echo $_SESSION['username'];}?>" name="username" placeholder="Username"><br/><br/>
                                 Email Address<br/><input type="text" value = "<?php if(isset($_SESSION['email_address'])){echo $_SESSION['email_address'];}?>" name="email_address" placeholder="Username"><br/><br/>
-                                Phone Number<br/><input type="text" value = "<?php if(isset($_SESSION['phone'])){echo $_SESSION['phone'];}?>" name="phone" placeholder="Phone Number"><br/><br/>
-                                Gender<br/><input type="text" value = "<?php if(isset($_SESSION['gender'])){echo $_SESSION['gender'];}?>" name="gender" placeholder="Gender"><br/><br/>
+                                Receive Email Notification ?<br/>
+                                <select name="receive_mail">
+                                    <option value="">Make Your Choice</option>
+                                    <?php
+                                        //$sql = "SELECT * FROM `categories`";
+                                        $res = $conn->query($sql);
+                                        while ($rows = mysqli_fetch_assoc($res)){ 
+                                        ?>
+                                        <option value="<?php echo $rows['category_id'];?>"><?php echo $rows['category_name'];?></option>
+
+                                        <?php
+                                        } 
+                                        ?>
+                                    </select> <br/><br/>
                                 <button class ="primary-button" type="submit" name="save">Save</button><br/><br/>
                                
                             </form>
