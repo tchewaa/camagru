c2c3161c72391b18df0be2c1378d43ae9f0bb787
Thabang <thabang.chewa@gmail.com>
2019-11-07 18:07:20 +0200

    show posts on logged user

--- profile.php
+++ profile.php
@@ -1,19 +1,6 @@
 
 <?php
-    require "includes/header.php";
-    if (isset($_POST['post']))
-    {
-        $filename = $_FILES['file']['name'];
-        $target_dir = "upload/";
-        $target_file = $target_dir . basename($_FILES["file"]["name"]);
-        // Select file type
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-        // Valid file extensions
-        $extensions_arr = array("jpg","jpeg","png","gif");
-        if(empty($filename)){
-            echo '<p class="error">Please attach an image to your post.</p>';
-        }
-    }
+    require("includes/header.php"); 
 ?>
 <main>
         <section class="timeline_section">
@@ -58,18 +45,37 @@
                  </div>
             </div>
 
-
                 <div class="row">
-                    <div class="timeline_feeds" style="width:100% !important">
-                       The Feeds
-
-                            
-                       
-
+                    <div class="timeline_feeds" style="width:100% !important; text-align:left">
+                       <?php
+                            try{
+                                $user_id = $_SESSION['user_id'];
+                                //$stmt = $conn->prepare("SELECT  *  FROM `image` WHERE `user_id` = 45");
+                                $stmt = $conn->prepare("SELECT u.user_id,  i.image_id, i.user_id, i.image_caption, i.image_name, i.image_time
+FROM users u, `image` i WHERE u.user_id = i.user_id AND u.user_id = $user_id AND i.user_id = $user_id ORDER BY i.image_time DESC"); 
+                                $stmt->execute();
+                                //$data = $stmt->fetch(PDO::FETCH_ASSOC);
+                                if ($stmt === false){                                            
+                                    $error = "NO POSTS YET...ADD SOMETHING.";
+                                }else{ 
+                                    foreach ($stmt as $row) {
+                                        echo "<div class='image-container'>";
+                                        echo "<img src='".$row['image_name']."' width='250px' height='250px' alt='Posts' class='image'>";
+                                        //echo    <img src="img_avatar.png" alt="Avatar" class="image">;
+                                        echo    "<div class='overlay'>";
+                                        echo        "<div class='text'>'".$row['image_caption']."'</div>";
+                                        echo    "</div>";
+                                        echo "</div>";
+                                        
+                                    }
+                                }
+                                   
+                            }catch(PDOException $e){
+                                    $error = "Error: ".$e->getMessage();
+                            }
                         
-
-
-
+                       ?>
+                       <?php if (isset($error)) echo $error ?>
                     </div>
                 </div>
             </div>
