9512d2009de638028ee8fc338042eef5b32a14bb
Thabang <thabang.chewa@gmail.com>
2019-11-07 18:06:41 +0200

    added explore link

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <?php
-require("config/setup.php");
+require("./db_connection.php");
 session_start();
-if(!isset($_SESSION['user_id']) && !isset($_SESSION['username']) && !isset($_SESSION['fullname']) && !isset($_SESSION['profile_pic']) && !isset($_SESSION['bio'])  && !isset($_SESSION['email_address']))
+if(!isset($_SESSION['user_id']) && !isset($_SESSION['username']) && !isset($_SESSION['password']) && !isset($_SESSION['fullname']) && !isset($_SESSION['profile_pic'])  && !isset($_SESSION['email_address']) && !isset($_SESSION['receive_email']))
 {
     header("Location: index.php");
 }
@@ -27,7 +27,7 @@ if(!isset($_SESSION['user_id']) && !isset($_SESSION['username']) && !isset($_SES
                             <p></p>
                         </div>
                         <ul>
-                            <li><a class="link" href="#"><i class="fa fa-indent" aria-hidden="true"></i> Explore</a></li>
+                            <li><a class="link" href="explore.php"><i class="fa fa-indent" aria-hidden="true"></i> Explore</a></li>
                             <li><a class="link" id ="myBtn"><i class="fa fa-list-alt" aria-hidden="true"></i>  Activity</a></li>
                             <li><a class="link" href="profile.php"><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
                             <li><a class="link" href="logout.php">Sign Out</a></li>
