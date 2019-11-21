658ae5c0c410b23e4205df3b23216e1de52ec635
tchewaa <50702994+tchewaa@users.noreply.github.com>
2019-11-21 12:13:33 +0200

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -33,7 +33,7 @@
                             $message = "
                             Hi Hi Camagruian!!!, <br/><br/>
                             Thank you for queries on Camagru, to reset your password, please click on the link below.<br/><br/>
-                            <a href='http://127.0.0.1:8080/camagru/reset_password.php?action=exists&email_address=$username_email&token=$token'>
+                            <a href='reset_password.php?action=exists&email_address=$username_email&token=$token'>
                             http://127.0.0.1:8080/camagru/reset_password.php?action=exists&email_address=$username_email&token=$token</a><br/><br/>
                             Kind Regards<br/><br/><br/>
                             Camagru Team!<br/>
