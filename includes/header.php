92d19b8e43af673049c77f7c0fb1bf2fcf8fe27e
Thabang Chewa <50702994+tchewaa@users.noreply.github.com>
2021-01-05 20:11:06 +0200

    css twicks and nav bar

--- includes/header.php
+++ includes/header.php
@@ -30,13 +30,14 @@ session_start();
                                if(!isset($_SESSION['user_id']) && !isset($_SESSION['username']) && !isset($_SESSION['password']) && !isset($_SESSION['fullname']) && !isset($_SESSION['profile_pic'])  && !isset($_SESSION['email_address']) && !isset($_SESSION['receive_email']))
                                {   
                                     echo '<li><a class="link" href="explore.php"><i class="fa fa-indent" aria-hidden="true"></i> Explore</a></li>';
-                                    echo '<li><a class="link" href="signin.php">Sign in</a></li>';
+                                    echo '<li><a class="link" href="new_account.php">Register</a></li>';
+                                    echo '<li><a class="link" href="signin.php">Login</a></li>';
 
                                 }else if(isset($_SESSION['user_id']) && isset($_SESSION['username']) && isset($_SESSION['password']) && isset($_SESSION['fullname']) && isset($_SESSION['profile_pic'])  && isset($_SESSION['email_address']) && isset($_SESSION['receive_email']))
                                {
                                      echo '<li><a class="link" href="explore.php"><i class="fa fa-indent" aria-hidden="true"></i> Explore</a></li>';
                                      echo '<li><a class="link" href="profile.php"><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>';
-                                     echo '<li><a class="link" href="logout.php">Sign Out</a></li>';
+                                     echo '<li><a class="link" href="logout.php">Logout</a></li>';
                                }else{
                                     header("Location: signin.php");
                                }
