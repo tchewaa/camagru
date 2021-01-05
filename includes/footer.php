7e6a60a3e3e62ddea189f853650d7005b2ad18a8
Thabang Chewa <50702994+tchewaa@users.noreply.github.com>
2021-01-05 20:44:14 +0200

    footer things

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
         <div class="container">
                 <div class="row">
                     <div class="footer" style="background: darkgrey; text-align:center; padding:20px">
-                        <p style="padding:20px">&copy; 2019 Camagru</p>
+                        <p style="padding:20px; color:white">&copy; 2021 Camagru</p>
                     </div>
                 </div>
         </div>
